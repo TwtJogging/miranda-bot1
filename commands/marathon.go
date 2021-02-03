9174d3803dff72c20c2d88503aa188636d7323f9
TwtJogging <67509703+TwtJogging@users.noreply.github.com>
2021-02-03 19:40:07 +0800

    Update marathon.go

--- commands/marathon.go
+++ commands/marathon.go
@@ -9,7 +9,7 @@ import (
 
 // Rules send rules
 func (c Command) Marathon() {
-	msg := tg.NewMessage(c.Message.Chat.ID, "<b>Senarai acara terkini larian marathon di Malaysia bagi tahun 2021.</b>-3 minit bacaan\n\nBaca: <a href='https://twitter.com/twt_jogging'>Read here!</a>\n\n1: Kedah Marathon -<b>ditangguhkan</b>\n\n1: Perlis Marathon -ditangguhkan\n\n1: Penang Marathon -<b>ditangguhkan</b>\n\n1: Kuala Kangsar Marathon -<b>ditangguhkan</b>")
+	msg := tg.NewMessage(c.Message.Chat.ID, "<b>Senarai acara terkini larian marathon di Malaysia bagi tahun 2021.</b>-3 minit bacaan\n\nBaca: <a href='https://twitter.com/twt_jogging'>Read here!</a>\n\n1: Kedah Marathon -<b>ditangguhkan</b>\n\n1: Perlis Marathon -<b>ditangguhkan</b>\n\n2: Penang Marathon -<b>ditangguhkan</b>\n\n3: Kuala Kangsar Marathon -<b>ditangguhkan</b>\n\n4: KLSCM Marathon -<b>ditangguhkan</b>\n\n5: TwinCity Marathon -<b>ditangguhkan</b>")
 	msg.ParseMode = "HTML"
 	msg.ReplyToMessageID = c.Message.MessageID
 
