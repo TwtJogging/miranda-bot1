1161c3f3d4097f0b6a0ee97d58646221fbb6e63c
TwtJogging <67509703+TwtJogging@users.noreply.github.com>
2021-02-03 10:44:03 +0800

    Update main.go

--- main.go
+++ main.go
@@ -372,7 +372,7 @@ func (app *App) handle(update tg.Update) {
 					}
 
 					text := fmt.Sprintf(
-						"Selamat datang [%s](tg://user?id=%d) 👋\n\nSila pastikan anda menaip semula dengan text berikut `%s` dalam waktu 5 minit untuk memastikan kamu bukan bot jahat.",
+						"Selamat datang [%s](tg://user?id=%d) 👋\n\nSila pastikan anda menaip semula dengan text berikut---> `%s` dalam waktu 5 minit untuk memastikan kamu bukan bot jahat.",
 						member.FirstName,
 						member.ID,
 						captcha.Code,
